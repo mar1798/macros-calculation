12815d3126951fd963ae4947ef1e163f479e07e8
anvar-drewl <anvar.muslimov@drewl.com>
2025-09-04 15:44:28 +0600

    feat: fixed next build and allow dev to be run

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,13 +1,7 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
 const withPlugins = require('next-compose-plugins')
-const { createSecureHeaders } = require('next-secure-headers')
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 const withVanillaExtract = createVanillaExtractPlugin()
-
-const envFallbackUrl = process.env.FALLBACK_BASE_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -36,51 +30,6 @@ const nextConfig = {
     ]
     return config
   },
-  rewrites,
-  headers,
-  redirects,
 }
 
-async function redirects() {
-  return [
-    {
-      source: '/sitemap.xml',
-      destination: '/sitemap_index.xml',
-      permanent: true,
-    },
-    {
-      source: '/blog/:slug+',
-      destination: '/resources/:slug+',
-      permanent: true,
-    },
-  ]
-}
-
-async function rewrites() {
-  return {
-    beforeFiles: [],
-    afterFiles: [
-      {
-        // yoast sitemaps
-        source: '/sitemap_index.xml',
-        destination: `${envFallbackUrl}/sitemap_index.xml`,
-      },
-      {
-        // yoast sitemaps
-        source: '/:sitemap([^/]*sitemap\\d*\\.xml)',
-        destination: `${envFallbackUrl}/:sitemap`,
-        basePath: false,
-      },
-      {
-        source: '/robots.txt',
-        destination: '/api/robots',
-      },
-    ],
-  }
-}
-
-async function headers() {
-  return [{ source: '/(.*)', headers: createSecureHeaders() }]
-}
-
-module.exports = withPlugins([withBundleAnalyzer, withVanillaExtract], nextConfig)
+module.exports = withPlugins([withVanillaExtract], nextConfig)
